aa20b8d570c27e6e8b778d33e7c87a49da2a3414
strawmelon11 <ouyangjn@foxmail.com>
2024-07-10 15:32:13 +0800

    revise code and update Readme

--- README.md
+++ README.md
@@ -10,8 +10,18 @@ The scores are in scale of 0-10.
 
 ` Boring, depressing`  high score indicates strong **negative** feeling
 
+Model Accuracy：
+| Model | safe | lively | wealthy | beautiful | boring | depressing |
+| :---: | :---: | :---: | :---: | :---: | :---: | :---: |
+| Accuracy | 76.7% | 77.1% | 72.9% | 76.9% | 61.6% | 67.2% |
+
+
 ## Model
-We used vision transformer model pretrianed on ImageNet (ViT_B_16_Weights.IMAGENET1K_SWAG_E2E_V1) as base model, and add 3 Linear layers in ViT heads for classification.
+The models are pre-trained on the MIT Place Pulse 2.0 dataset. The backbone of the models are vision transformer (ViT) pretrianed on ImageNet (ViT_B_16_Weights.IMAGENET1K_SWAG_E2E_V1). 
+
+In the ViT heads, 3 Linear layers with ReLU activiation are added for classification.
+
+
 
 ## How to run the model
 Install packages from requirements.txt
@@ -21,9 +31,9 @@ Install packages from requirements.txt
 Change the file path in *eval.py*
 
 ```
-model_load_path = "D:/model"   # model path
-images_path = "D:/test_image"      # your input image path
-out_Path = "D:/output"     # output path
+model_load_path = "./model"   # model path
+images_path = "./test_image"      # input image path
+out_Path = "./output"     # output path
 ```
 Run the file *eval.py*
 
--- eval.py
+++ eval.py
@@ -1,30 +1,29 @@
-# coding=UTF-8  
+# coding=UTF-8
 import os
 import pandas as pd
-import torch 
+import torch
 import torch.nn as nn
 from torchvision import transforms as T
 from PIL import Image
+from transformers import AutoModel
+from huggingface_hub import snapshot_download
 
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-perception = ['safety', 'lively', 'wealthy', 'beautiful', 'boring', 'depressing']
+perception = ['safety', 'lively', 'wealthy',
+              'beautiful', 'boring', 'depressing']
 model_dict = {
-            'safety':'safety.pth', \
-            'lively': 'lively.pth', \
-            'wealthy': 'wealthy.pth',\
-            'beautiful':'beautiful.pth',\
-            'boring': 'boring.pth',\
-            'depressing': 'depressing.pth',\
-            }
+    'safety': 'safety.pth',
+    'lively': 'lively.pth',
+    'wealthy': 'wealthy.pth',
+    'beautiful': 'beautiful.pth',
+    'boring': 'boring.pth',
+    'depressing': 'depressing.pth',
+}
 
-
-model_load_path = "D:/model"   # model dir path
-images_path = "D:/test_image"      # input image path
-out_Path = "D:/output"     # output path
 
 train_transform = T.Compose([
-    T.Resize((384,384)),
+    T.Resize((384, 384)),
     T.ToTensor(),
     T.Normalize(
         mean=[0.485, 0.456, 0.406],
@@ -46,37 +45,49 @@ def predict(model, img_path, device):
     softmax = nn.Softmax(dim=1)
     pred = softmax(pred)[0][1].item()
     pred = round(pred*10, 2)
-    
+
     return pred
 
 
-
 if __name__ == "__main__":
+    
+    model_load_path = "./model"   # model dir path
+    images_path = "./test_image"      # input image path
+    out_Path = "./output"     # output path
+    
+    # download model
+    print("Downloading models ...")
+    snapshot_download(repo_id="Jiani11/human-perception-place-pulse",
+                      allow_patterns=["*.pth", "README.md"], local_dir=model_load_path)
+
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print("using device:{} ".format(device))
     for p in perception:
         # load model
-        model_path = model_load_path + "/" +  model_dict[p]
-        model = torch.load(model_path, map_location=torch.device(device))  
+        model_path = model_load_path + "/" + model_dict[p]
+        model = torch.load(model_path, map_location=torch.device(device))
+        if torch.cuda.device_count() > 1:
+            model = nn.DataParallel(model)
+        model = model.to(device)
         print("######### {}  #########".format(p))
-        if device == 'cuda:0':
-            model = model.to(device)
-        # print(model)
         model.eval()
+        
+        # Check if the directory exists, if not, create it
+        if not os.path.exists(out_Path):
+            os.makedirs(out_Path)
+    
+        # inferring img
         out_csvPath = out_Path + "/" + str(p) + ".csv"
-        df = pd.DataFrame(columns=['img_path',str(p)+"_score"])
+        df = pd.DataFrame(columns=['img_path', str(p)+"_score"])
         df.to_csv(out_csvPath, index=False)
         data_arr = []
-        # img = images_path + "/1000001600535997.JPG"
-        for img in os.listdir(images_path): 
+
+        for img in os.listdir(images_path):
             img_path = images_path + "/" + img
-            print("current image: ",img_path)
-            score = predict(model,img_path,device)
-            data_arr = [img,score]
-            df=pd.DataFrame(data_arr).T
-            df.to_csv(out_csvPath, mode='a', header=False, index=False)  # save scores into csv
-        print("{} done!".format(p))
-
-
-        
-    
+            print("current image: ", img_path)
+            score = predict(model, img_path, device)
+            data_arr = [img, score]
+            df = pd.DataFrame(data_arr).T
+            df.to_csv(out_csvPath, mode='a', header=False,
+                      index=False)  # save scores into csv
+        print("{} prediction done!".format(p))
